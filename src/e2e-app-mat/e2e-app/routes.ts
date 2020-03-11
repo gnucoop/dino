d83585cc70de3af729f01b20f85b487498b1e901
trik <marco.marche@gnucoop.com>
2020-03-11 18:49:40 +0100

    refactor: join ion and mat e2e apps in single app

--- src/e2e-app-mat/e2e-app/routes.ts
+++
@@ -1,6 +0,0 @@
-import {Routes} from '@angular/router';
-import {Home} from './e2e-app-layout';
-
-export const E2E_APP_ROUTES: Routes = [
-  {path: '', component: Home},
-];
--- src/e2e-app/e2e-app/e2e-app-module.ts
+++ src/e2e-app/e2e-app/e2e-app-module.ts
@@ -1,34 +1,39 @@
 /**
  * @license
- * Copyright (C) 2020 Gnucoop soc. coop.
+ * Copyright (C) Gnucoop soc. coop.
  *
- * This file is part of the Dewco (dewco).
+ * This file is part of the Advanced JSON forms (ajf).
  *
- * Dewco (dewco) is free software: you can redistribute it and/or
+ * Advanced JSON forms (ajf) is free software: you can redistribute it and/or
  * modify it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
  *
- * Dewco (dewco) is distributed in the hope that it will be useful,
+ * Advanced JSON forms (ajf) is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
  * General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with Dewco (dewco).
+ * along with Advanced JSON forms (ajf).
  * If not, see http://www.gnu.org/licenses/.
  *
  */
 
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
-import {ExampleListViewer} from './example-list-viewer.component';
-import {ExampleViewer} from './example-viewer';
+import {MatListModule} from '@angular/material/list';
+import {RouterModule} from '@angular/router';
+import {E2eAppLayout, Home} from './e2e-app-layout';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [ExampleViewer, ExampleListViewer],
-  exports: [ExampleViewer, ExampleListViewer]
+  imports: [
+    CommonModule,
+    MatListModule,
+    RouterModule,
+  ],
+  declarations: [E2eAppLayout, Home],
+  exports: [E2eAppLayout],
 })
-export class ExampleViewerModule {
+export class E2eAppModule {
 }
