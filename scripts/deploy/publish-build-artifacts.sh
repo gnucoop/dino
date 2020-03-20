e1a64a5773dbe654fd3a4ab4f52fcc230b09b446
trik <marco.marche@gnucoop.com>
2020-03-20 09:12:55 +0100

    build: use sed in publish build artifacts script
    gsed is not a valid command in linux

--- scripts/deploy/publish-build-artifacts.sh
+++ scripts/deploy/publish-build-artifacts.sh
@@ -96,7 +96,7 @@ publishPackage() {
   # the SHA of the current build job. Normally this "sed" call would just replace the version
   # placeholder, but the version placeholders have been replaced by "npm_package" already.
   escapedVersion=$(echo ${buildVersion} | sed 's/[.[\*^$]/\\&/g')
-  gsed -i "s/${escapedVersion}/${buildVersionName}/g" $(find . -type f -not -path '*\/.*')
+  sed -i "s/${escapedVersion}/${buildVersionName}/g" $(find . -type f -not -path '*\/.*')
 
   echo "Updated the build version in every file to include the SHA of the latest commit."
 
