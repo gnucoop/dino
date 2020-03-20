9a317009e9e581677e9d13f4a4a7e213ba80307a
trik <marco.marche@gnucoop.com>
2020-03-20 10:04:27 +0100

    build: skip deploy if current commit has no version tag

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,19 +9,23 @@ BASE_DIR="${PWD}/dist/releases"
 GIT_BRANCH=master
 
 CUR_TAG=`git describe --abbrev=0 --tags --exact-match 2> /dev/null`
-TAG_VERSION=`yarn semver ${CUR_TAG}`
+if [[ -z $CUR_TAG ]]; then
+  echo "Skipping deploy of ${PACKAGE} package"
+else
+  TAG_VERSION=`yarn semver ${CUR_TAG}`
 
-for PACKAGE in ${PACKAGES[@]}
-do
-  PACKAGE_DIR="${BASE_DIR}/${PACKAGE}"
-  CURRENT_VERSION=`npm show @dewco/${PACKAGE} version`
-  echo $CURRENT_VERSION
-  echo $TAG_VERSION
-  if [[ "${CURRENT_VERSION}" == "${TAG_VERSION}" ]]; then
-    echo "Skipping deploy of ${PACKAGE} package"
-  else
-    cp "${PWD}/.npmrc" $PACKAGE_DIR
-    cd $PACKAGE_DIR
-    npm publish
-  fi
-done
+  for PACKAGE in ${PACKAGES[@]}
+  do
+    PACKAGE_DIR="${BASE_DIR}/${PACKAGE}"
+    CURRENT_VERSION=`npm show @dewco/${PACKAGE} version`
+    echo $CURRENT_VERSION
+    echo $TAG_VERSION
+    if [[ "${CURRENT_VERSION}" == "${TAG_VERSION}" ]]; then
+      echo "Skipping deploy of ${PACKAGE} package"
+    else
+      cp "${PWD}/.npmrc" $PACKAGE_DIR
+      cd $PACKAGE_DIR
+      npm publish
+    fi
+  done
+fi
