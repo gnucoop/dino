6312dba56e54e28975393780b3454058a20a336e
Marco Tozzi <marco.tozzi@gnucoop.com>
2020-10-08 16:07:20 +0200

    feat(material/SearchFiltersPresetManager): Created material SearchFiltersPresetManager component
    A component that can save and load user defined presets of filters.
    Presets are stored in the localStorage as encoded strings, containing the queryParams
    for the url.

---
+++ src/material/searchfilters-preset-manager/search-filters-preset-manager.module.ts
@@ -0,0 +1,57 @@
+/**
+ * @license
+ * Copyright (C) Gnucoop soc. coop.
+ *
+ * This file is part of the Dewco (dewco).
+ *
+ * Dewco (dewco) is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * Dewco (dewco) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Dewco (dewco).
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule} from '@angular/router';
+
+import {SearchFiltersPresetManager} from './search-filters-preset-manager';
+
+@NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    CommonModule,
+    FormsModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    RouterModule,
+  ],
+  declarations: [
+    SearchFiltersPresetManager,
+  ],
+  exports: [
+    SearchFiltersPresetManager,
+  ],
+})
+export class SearchFiltersPresetManagerModule {
+}
---
+++ src/material/searchfilters-preset-manager/search-filters-preset-manager.spec.ts
@@ -0,0 +1,113 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {ActivatedRoute} from '@angular/router';
+import {RouterTestingModule} from '@angular/router/testing';
+import {FilterItem, FiltersService} from '@dewco/core/list';
+import {SearchFiltersPresetManager} from '@dewco/material/searchfilters-preset-manager';
+import {of as obsOf} from 'rxjs';
+
+const fakeFilters: FilterItem[] = [
+  {name: 'filter_a', value: 'test'},
+  {name: 'filter_b', value: 15},
+  {name: 'filter_c', value: false},
+];
+
+const fakeFiltersPreset = btoa(encodeURI(JSON.stringify(fakeFilters)));
+
+const fakeActivatedRoute = {
+  queryParams: obsOf({'filters': fakeFiltersPreset})
+} as unknown as ActivatedRoute;
+
+describe('Search filters Bar', () => {
+  let fts: FiltersService;
+  let fixturePresetManager: ComponentFixture<SearchFiltersPresetManager>;
+  let presetManager: SearchFiltersPresetManager;
+
+  beforeEach(() => {
+    TestBed
+        .configureTestingModule({
+          declarations: [
+            SearchFiltersPresetManager,
+          ],
+          imports: [
+            MatAutocompleteModule,
+            RouterTestingModule,
+          ],
+          providers: [
+            {provide: ActivatedRoute, useValue: fakeActivatedRoute},
+          ],
+        })
+        .compileComponents();
+
+    fts = TestBed.inject(FiltersService);
+    fixturePresetManager = TestBed.createComponent(SearchFiltersPresetManager);
+    presetManager = fixturePresetManager.componentInstance;
+  });
+
+  it('should create the component', () => {
+    fixturePresetManager.detectChanges();
+
+    expect(presetManager).toBeTruthy();
+    expect(fts).toBeTruthy();
+  });
+
+  it('should load the preset data on init', async () => {
+    fixturePresetManager.detectChanges();
+
+    const presetData = await presetManager.presetData.toPromise();
+    expect(presetData).not.toBeNull();
+    expect(presetData).toEqual(fakeFiltersPreset);
+  });
+
+  it('should save a preset in the localStorage', () => {
+    fixturePresetManager.detectChanges();
+    const spySetItem = spyOn(localStorage, 'setItem').and.callThrough();
+
+    presetManager.presetControl.setValue('test_preset');
+    presetManager.savePreset();
+    const item = localStorage.getItem('filters_preset_test_preset');
+
+    expect(spySetItem).toHaveBeenCalled();
+    expect(item).toEqual(fakeFiltersPreset);
+  });
+
+  it('should NOT save a nameless preset in the localStorage', () => {
+    fixturePresetManager.detectChanges();
+    const spySetItem = spyOn(localStorage, 'setItem').and.callThrough();
+
+    presetManager.presetControl.setValue('');
+    presetManager.savePreset();
+
+    expect(spySetItem).not.toHaveBeenCalled();
+  });
+
+  it('should retrieve a preset from the localStorage and ask the FilterService to load it', () => {
+    fixturePresetManager.detectChanges();
+    const spyGetItem = spyOn(localStorage, 'getItem').and.callThrough();
+    const spyFtsLoad = spyOn(fts, 'loadPreset').and.callThrough();
+
+    presetManager.presetControl.setValue('test_preset');
+    presetManager.savePreset();
+
+    presetManager.loadPreset();
+
+    expect(spyGetItem).toHaveBeenCalled();
+    expect(spyGetItem).toHaveBeenCalledWith('filters_preset_test_preset');
+    expect(spyFtsLoad).toHaveBeenCalled();
+    expect(spyFtsLoad).toHaveBeenCalledWith(fakeFiltersPreset);
+  });
+
+  it('should NOT try to retrieve a non existing preset', () => {
+    fixturePresetManager.detectChanges();
+    const spyGetItem = spyOn(localStorage, 'getItem').and.callThrough();
+    const spyFtsLoad = spyOn(fts, 'loadPreset').and.callThrough();
+
+    presetManager.presetControl.setValue('test_preset');
+    presetManager.savePreset();
+    presetManager.presetControl.setValue('test_wrong_preset');
+    presetManager.loadPreset();
+
+    expect(spyGetItem).not.toHaveBeenCalled();
+    expect(spyFtsLoad).not.toHaveBeenCalled();
+  });
+});
