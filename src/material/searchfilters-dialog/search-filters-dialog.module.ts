88cb49bb504e86e774ab8bf88da49d87cb094846
Marco Tozzi <marco.tozzi@gnucoop.com>
2020-10-08 15:51:23 +0200

    feat(material/SearchFiltersDialog): Created material SearchFilterDialog component
    A Dialog displaying all advanced filters, based on the List model or custom.
    Displays individual filter widgets grouped in tabs.
    Can be associated to a SearchFiltersBar component.

---
+++ src/material/searchfilters-dialog/search-filters-dialog.module.ts
@@ -0,0 +1,67 @@
+/**
+ * @license
+ * Copyright (C) Gnucoop soc. coop.
+ *
+ * This file is part of the Dewco (dewco).
+ *
+ * Dewco (dewco) is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * Dewco (dewco) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Dewco (dewco).
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatTabsModule} from '@angular/material/tabs';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule} from '@angular/router';
+import {SearchFiltersChipsModule} from '@dewco/material/searchfilters-chips';
+import {SearchFiltersWidgetModule} from '@dewco/material/searchfilters-widget';
+
+import {SearchFiltersDialog} from './search-filters-dialog';
+
+@NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    CommonModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatTabsModule,
+    RouterModule,
+    SearchFiltersChipsModule,
+    SearchFiltersWidgetModule,
+  ],
+  declarations: [
+    SearchFiltersDialog,
+  ],
+  entryComponents: [
+    SearchFiltersDialog,
+  ],
+  exports: [
+    SearchFiltersDialog,
+  ],
+})
+export class SearchFiltersDialogModule {
+}
---
+++ src/material/searchfilters-dialog/search-filters-dialog.ts
@@ -0,0 +1,241 @@
+/**
+ * @license
+ * Copyright (C) Gnucoop soc. coop.
+ *
+ * This file is part of the Dewco (dewco).
+ *
+ * Dewco (dewco) is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * Dewco (dewco) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Dewco (dewco).
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import {
+  AjfChoicesOrigin,
+  AjfFieldType,
+  AjfForm,
+  AjfFormSerializer,
+  AjfNode,
+  AjfNodeType,
+  AjfSlide
+} from '@ajf/core/forms';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Inject,
+  OnDestroy,
+  OnInit,
+  QueryList,
+  ViewChildren,
+  ViewEncapsulation,
+} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {
+  DEFAULT_OPERATORS,
+  FilterGroup,
+  FilterItem,
+  filterListType,
+  FiltersService,
+  WidgetData
+} from '@dewco/core/list';
+import {BehaviorSubject, Observable, Subscription} from 'rxjs';
+import {map, take, tap, withLatestFrom} from 'rxjs/operators';
+
+import {SearchFiltersWidget} from '../searchfilters-widget';
+
+/**
+ * Dialog component that shows and handles all advancedFilters.
+ * It may contain dewco-mat-searchfilters-chips and multiple dewco-mat-searchfilters-widget.
+ */
+@Component({
+  selector: 'dewco-mat-searchfilters-dialog',
+  styleUrls: ['search-filters-dialog.css'],
+  templateUrl: 'search-filters-dialog.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+})
+export class SearchFiltersDialog implements OnInit, OnDestroy, AfterViewInit {
+  /**
+   * The Id of the selected tab
+   */
+  private _currentGroupId: BehaviorSubject<number>;
+
+  private _updateWidgetsEvent: EventEmitter<boolean>;
+
+  /**
+   * Data of all widgets in the dialog
+   */
+  widgetData: Observable<WidgetData[]>;
+
+  private _updateWidgetsSub: Subscription = Subscription.EMPTY;
+
+  @ViewChildren(SearchFiltersWidget) widgets: QueryList<SearchFiltersWidget>;
+
+  constructor(
+      public dialogRef: MatDialogRef<SearchFiltersDialog>,
+      @Inject(MAT_DIALOG_DATA) public data: any,
+      public fts: FiltersService,
+  ) {
+    this._currentGroupId = new BehaviorSubject<number>(0);
+    this._updateWidgetsEvent = new EventEmitter<boolean>();
+    this.dialogRef.backdropClick().subscribe(_ => this.closeDialog());
+  }
+
+  ngOnInit() {
+    this.widgetData = this._currentGroupId.pipe(
+        withLatestFrom(this.fts.modelFilters),
+        map(([id, groups]) => groups[id] as FilterGroup),
+        map((group) => group.filterGroupAdvancedFilters ?
+                group.filterGroupAdvancedFilters.filter(ft => ft.fieldType !== AjfFieldType.Empty) :
+                []),
+        map(filters => filters.map(f => this._setupWidget(this._setupFilterItem(f)))),
+        take(1),
+    );
+  }
+
+  ngAfterViewInit() {
+    if (this.widgets) {
+      this._updateWidgetsSub = this._updateWidgetsEvent.asObservable().subscribe(_ => {
+        let matchingWidgets = this.widgets.toArray().filter(widget => {
+          return this.fts.temporaryFilters.value.some(
+              ft => ft.name === widget.widgetName && ft.value === null &&
+                  widget.toggleButton.checked);
+        });
+        matchingWidgets.map(w => {
+          w.toggleButton.toggle();
+        });
+      });
+    }
+  }
+
+  /**
+   * Closes the dialog and resets the Temporary Filters
+   *
+   */
+  closeDialog() {
+    this.dialogRef.close(false);
+  }
+
+  /**
+   * Closes the dialog and updates the Active Filters
+   *
+   */
+  search() {
+    this.dialogRef.close(true);
+  }
+
+  /**
+   * Sets the group id in the mat tab group
+   * @param {number} id
+   *
+   */
+  setCurrentGroupId(id: number): void {
+    this._currentGroupId.next(id);
+  }
+
+  /**
+   * Asks the FilterService to add a FilterItem to the filterItems list of the chosen filterListType
+   * @param {FilterItem} filterItem
+   * @param {filterListType} filterList
+   */
+  addFilter(filterItem: FilterItem, listType: filterListType): void {
+    if (filterItem.value !== null && filterItem.value !== '') {
+      this.fts.addFilter(filterItem, listType);
+    }
+  }
+
+  /**
+   * Asks the FilterService to remove a FilterItem from the filterItems lists of the chosen
+   * filterListTypes, then triggers the update of the associated widgets.
+   * @param {FilterItem} filterItem
+   * @param {filterListType} filterList
+   */
+  removeFilter(filterItem: FilterItem, listType: filterListType[]|filterListType): void {
+    this.fts.removeFilter(filterItem, listType)
+        .pipe(take(1))
+        .subscribe(_ => this._updateWidgetsEvent.emit(true));
+  }
+
+
+  /**
+   * Generates and sets up a FilterItem
+   * @param {FilterItem} item
+   * @return {FilterItem}
+   */
+  private _setupFilterItem(item: FilterItem): FilterItem {
+    const ftItem: FilterItem = {
+      id: item.id ?? 11,
+      parent: 1,
+      parentNode: item.parentNode ?? 1,
+      choicesOrigin: item.choicesOrigin,
+      choicesOriginRef: item.choicesOrigin?.name,
+      name: item.name,
+      label: item.label ?? item.name.charAt(0).toUpperCase() + item.name.slice(1),
+      nodeType: AjfNodeType.AjfField,
+      fieldType: item.fieldType ? item.fieldType : AjfFieldType.String,
+      isFormData: item.isFormData,
+      editable: item.editable ?? true,
+      defaultValue: item.defaultValue ?? null,
+      size: item.size ?? 'normal',
+      validation: item.validation,
+      visibility: item.visibility != null ? item.visibility : {condition: 'true'},
+    };
+    return ftItem;
+  }
+
+  /**
+   * Transforms a FilterItem into a WidgetData object
+   * @param {FilterItem} filterItem
+   * @returns {WidgetData}
+   */
+  private _setupWidget(filterItem: FilterItem): WidgetData {
+    const activeFilter = this.fts.temporaryFilters.value.find(f => f.name === filterItem.name);
+    const fieldValue = activeFilter ? activeFilter.value : null;
+    const fieldChoices: AjfChoicesOrigin<any>[] =
+        filterItem.choicesOrigin ? [filterItem.choicesOrigin] : [];
+    filterItem.value = fieldValue;
+    filterItem.operator =
+        activeFilter?.operator ?? DEFAULT_OPERATORS[filterItem.fieldType ?? AjfFieldType.String];
+    const formSchema: Partial<AjfForm> = {
+      choicesOrigins: fieldChoices,
+      nodes: [{
+        parent: 0,
+        parentNode: 0,
+        id: 1,
+        name: filterItem.name,
+        label: filterItem.label,
+        nodeType: AjfNodeType.AjfSlide,
+        nodes: [filterItem as Partial<AjfNode>]
+      } as AjfSlide],
+    };
+    const ctx = Object.create({});
+    ctx[filterItem.name] = fieldValue;
+    const filterVisibility = filterItem.visibility;
+    const form = AjfFormSerializer.fromJson(formSchema, ctx);
+    form.nodes[0].visibility = filterVisibility;
+    return {
+      form: form,
+      operator: filterItem.operator,
+      active: fieldValue != null,
+      validation: filterItem.validation,
+      isFormData: filterItem.isFormData ?? false,
+    };
+  }
+
+  ngOnDestroy() {
+    this._updateWidgetsSub.unsubscribe();
+    this._currentGroupId.complete();
+  }
+}
