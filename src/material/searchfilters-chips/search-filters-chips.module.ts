03983f027cb9db154535a9f9241c11dee3bb17d9
Marco Tozzi <marco.tozzi@gnucoop.com>
2020-10-08 15:56:40 +0200

    feat(material/SearchFiltersChips): Created material SearchFiltersChips component
    Component that displays all the currently selected filters, their values and
    operators, as mat-chips. Can be associated to a SearchFiltersDialog component or
    to a SearchFiltersBar component.

---
+++ src/material/searchfilters-chips/search-filters-chips.module.ts
@@ -0,0 +1,50 @@
+/**
+ * @license
+ * Copyright (C) Gnucoop soc. coop.
+ *
+ * This file is part of the Dewco (dewco).
+ *
+ * Dewco (dewco) is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * Dewco (dewco) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Dewco (dewco).
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatInputModule} from '@angular/material/input';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule} from '@angular/router';
+
+import {SearchFiltersChips} from './search-filters-chips';
+
+@NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    CommonModule,
+    MatButtonModule,
+    MatChipsModule,
+    MatInputModule,
+    RouterModule,
+  ],
+  declarations: [
+    SearchFiltersChips,
+  ],
+  exports: [
+    SearchFiltersChips,
+  ],
+})
+export class SearchFiltersChipsModule {
+}
---
+++ src/material/searchfilters-chips/search-filters-chips.spec.ts
@@ -0,0 +1,46 @@
+import {ChangeDetectorRef} from '@angular/core';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {FilterItem, FiltersService} from '@dewco/core/list';
+import {SearchFiltersChips} from '@dewco/material/searchfilters-chips';
+
+const changeDetectorRefMock = {
+  markForCheck() {}
+};
+
+const fakeFilter: FilterItem = {
+  name: 'filter_a',
+  value: 'test',
+  operator: {label: 'Like', value: '$regex'}
+};
+
+describe(
+    'Searchfilters Chips. Displays all active filters as MatChips, allowing their deletion', () => {
+      let fts: FiltersService;
+      let fixture: ComponentFixture<SearchFiltersChips>;
+      let chips: SearchFiltersChips;
+
+      beforeEach(() => {
+        TestBed
+            .configureTestingModule({
+              declarations: [SearchFiltersChips],
+              imports: [RouterTestingModule],
+              providers: [
+                FiltersService,
+                {provide: ChangeDetectorRef, useValue: changeDetectorRefMock},
+              ],
+            })
+            .compileComponents();
+        fts = TestBed.inject(FiltersService);
+        fixture = TestBed.createComponent(SearchFiltersChips);
+        chips = fixture.componentInstance;
+      });
+
+      it('should emit the excludeFilter event', () => {
+        const spyExcludeFilter = spyOn<any>(chips.excludeFilter, 'emit').and.callThrough();
+        chips.removeFilterItem(fakeFilter);
+        fixture.detectChanges();
+
+        expect(spyExcludeFilter).toHaveBeenCalledWith(fakeFilter);
+      });
+    });
