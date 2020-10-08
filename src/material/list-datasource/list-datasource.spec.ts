d79f6fa7f79a7b52edd9618a856098e000228d42
Marco Tozzi <marco.tozzi@gnucoop.com>
2020-10-08 15:41:38 +0200

    feat(material/listDataSource): Create a custom DataSource for SelectionList
    A custom DataSource extending and augmenting MatTableDataSource.
    Provides data to SelectionList, generating a query for the DataManager
    based on the current active filters.

---
+++ src/material/list-datasource/list-datasource.spec.ts
@@ -0,0 +1,183 @@
+import {TestBed} from '@angular/core/testing';
+import {ActivatedRoute, Router} from '@angular/router';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AuthService, User} from '@dewco/core/auth';
+import {
+  DATA_SERVICE_CONFIG,
+  DataCreateCollectionRequest,
+  DataModelManager,
+  DataQueryOptions,
+  DataService,
+  DataServiceConfig,
+  Model,
+  PermissionContextService
+} from '@dewco/core/data';
+import {
+  FilterItem,
+  FiltersService,
+} from '@dewco/core/list';
+import {ListDataSource} from '@dewco/material/list-datasource';
+import {RxJsonSchema} from 'rxdb';
+import {of as obsOf} from 'rxjs';
+
+interface DummyModel extends Model {
+  name: string;
+  filter_a?: string;
+  filter_b?: number;
+  filter_c?: boolean;
+}
+
+const dummySchema = {
+  'type': 'object',
+  'properties': {
+    'id': {'type': 'string', 'description': 'UUID v4 identifier.'},
+    'name': {'type': 'string', 'description': 'Element name'},
+    'filter_a': {'type': 'string', 'description': ''},
+    'filter_b': {'type': 'number', 'description': ''},
+    'filter_c': {'type': 'boolean', 'description': ''},
+    'created_at': {'type': 'string', 'description': 'Creation timestamp.'},
+    'updated_at': {'type': 'string', 'description': 'Update timestamp.'},
+  },
+  'additionalProperties': false,
+  'title': 'dummymodel',
+  'version': 0,
+} as RxJsonSchema;
+
+const createCollectionParams = {
+  collection: {
+    name: 'dummymodel',
+    schema: dummySchema,
+  }
+};
+
+const dummyUser: User = {
+  id: 'userid',
+  email: 'user@dewco.gnu',
+  firstName: 'dummy',
+  lastName: 'dewco',
+  active: true,
+  verified: true,
+  tenantId: '1',
+  insertInstant: 1,
+  lastLoginInstant: 1,
+  passwordChangeRequired: false,
+  passwordLastUpdateInstant: 1,
+  twoFactorEnabled: false,
+  twoFactorDelivery: 'None',
+  usernameStatus: 'ACTIVE',
+  registrations: []
+};
+
+const authServiceMock = {
+  authenticated: obsOf(true),
+  getUserInfo: () => {
+    return dummyUser;
+  },
+} as AuthService;
+
+const fakeFilters: FilterItem[] = [
+  {name: 'filter_a', value: 'test', operator: {label: 'Like', value: '$regex'}},
+  {name: 'filter_b', value: 15, operator: {label: '>=', value: '$gte'}},
+  {name: 'filter_c', value: false, operator: {label: '==', value: '$eq'}},
+];
+
+const fakeFiltersPreset = btoa(encodeURI(JSON.stringify(fakeFilters)));
+
+const fakeActivatedRoute = {
+  queryParams: obsOf({'filters': fakeFiltersPreset})
+} as unknown as ActivatedRoute;
+
+let testDbIdx = 0;
+
+function dataServiceConfig(): DataServiceConfig {
+  return {
+    databaseCreateOptions: {
+      name: `dewco_datamanager_test_db_${testDbIdx++}`,
+      adapter: 'memory',
+    },
+    syncOptions: {
+      url: 'host',
+    },
+  };
+}
+
+class DummyManager extends DataModelManager<DummyModel> {
+  constructor(
+      createParams: DataCreateCollectionRequest,
+      dataService: DataService,
+      permissionContextService: PermissionContextService,
+  ) {
+    super(createParams, dataService, permissionContextService);
+  }
+}
+
+
+describe('ListComponent', () => {
+  let dummyManager: DummyManager;
+  let dataService: DataService;
+  let contextService: PermissionContextService;
+  let fts: FiltersService;
+  let route: ActivatedRoute;
+  let router: Router;
+  let dataSource: ListDataSource<DummyModel, DummyManager>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+      ],
+      providers: [
+        {provide: FiltersService, useValue: fts},
+        {provide: ActivatedRoute, useValue: fakeActivatedRoute},
+        {provide: DummyManager, useValue: dummyManager},
+        {provide: AuthService, useValue: authServiceMock},
+        {provide: DATA_SERVICE_CONFIG, useValue: dataServiceConfig()},
+      ],
+    });
+    contextService = TestBed.inject(PermissionContextService);
+    dataService = TestBed.inject(DataService);
+    dummyManager = new DummyManager(createCollectionParams, dataService, contextService);
+    route = TestBed.inject(ActivatedRoute);
+    router = TestBed.inject(Router);
+    fts = new FiltersService(route, router);
+    dataSource = new ListDataSource<DummyModel, DummyManager>(dummyManager, fts);
+  });
+
+  describe('ListDataSource extending MatDataSource. Queries the indexedDb for data', () => {
+    it('should create a Mango Query from an encoded queryString', () => {
+      const spyAddNestedProp = spyOn<any>(dataSource, '_addNestedProps').and.callThrough();
+      const spyQueryResults = spyOn(dataSource, 'getQueryResults').and.callThrough();
+
+      const query = dataSource.queryDM(fakeFiltersPreset);
+      const expectedMangoQuery: DataQueryOptions = {
+        selector: {
+          filter_a: {$regex: 'test'},
+          filter_b: {$gte: 15},
+          filter_c: {$eq: false},
+        },
+      };
+
+      expect(spyAddNestedProp).toHaveBeenCalled();
+      expect(spyQueryResults).toHaveBeenCalled();
+      expect(query).toEqual(expectedMangoQuery);
+    });
+
+    it('should query the dataModelManager', () => {
+      const spyDmQuery = spyOn(dummyManager, 'query').and.callThrough();
+      const mangoQuery: DataQueryOptions = {selector: {name: {$regex: 'it'}}};
+
+      dataSource.getQueryResults(mangoQuery);
+
+      expect(spyDmQuery).toHaveBeenCalledWith(mangoQuery);
+    });
+
+    it('should call the dataModelManager bulkDelete', () => {
+      const spyDmDelete = spyOn(dummyManager, 'bulkDelete').and.callThrough();
+      const items = [{name: 'item', id: '', created_at: '', updated_at: ''}];
+
+      dataSource.deleteAction(items);
+
+      expect(spyDmDelete).toHaveBeenCalledWith(items);
+    });
+  });
+});
---
+++ src/material/list-datasource/list-datasource.ts
@@ -0,0 +1,287 @@
+/**
+ * @license
+ * Copyright (C) Gnucoop soc. coop.
+ *
+ * This file is part of the Dewco (dewco).
+ *
+ * Dewco (dewco) is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * Dewco (dewco) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Dewco (dewco).
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort, Sort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import {
+  DataModelManager,
+  DataQueryOptions,
+  Model,
+} from '@dewco/core/data';
+import {
+  FilterItem,
+  FiltersService,
+  SearchFiltersComponent,
+} from '@dewco/core/list';
+import {RxDocument, RxJsonSchema} from 'rxdb';
+import {
+  BehaviorSubject,
+  combineLatest,
+  from,
+  Subscription,
+} from 'rxjs';
+import {map, switchMap, take} from 'rxjs/operators';
+
+
+export class ListDataSource<T extends Model = Model,
+                                      DM extends DataModelManager<T> = DataModelManager<T>> extends
+    MatTableDataSource<T> {
+  private _modelSchema: RxJsonSchema;
+  private _formSchema: object;
+  get modelSchema(): RxJsonSchema {
+    return this._modelSchema;
+  }
+  private _dataResults: BehaviorSubject<T[]> = new BehaviorSubject<T[]>([]);
+  private _sortParams: BehaviorSubject<Sort|null> = new BehaviorSubject<Sort|null>(null);
+  refreshList: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+
+  private _dataResultsSub: Subscription = Subscription.EMPTY;
+  private _filterParamsSub: Subscription = Subscription.EMPTY;
+
+  constructor(private _dataModelManager: DM, private _fs: FiltersService, formSchema?: any) {
+    // @TODO (Marco):  formSchema will be provided by FormSchemaManager
+    super();
+
+    /**
+     * The model RxJsonSchema
+     */
+    this._modelSchema = this._dataModelManager.collectionSchema;
+
+    /**
+     * The AjfFormSchema
+     */
+    this._formSchema = formSchema;
+    this._dataResultsSub = this._dataResults.subscribe(results => {
+      this.data = results;
+    });
+
+    /**
+     * Asks the FilterService to generate all the default filters, providing the model and, if
+     * present, form schema.
+     */
+    this._fs.generateFilters(this._modelSchema, this._formSchema);
+
+    this._filterParamsSub =
+        combineLatest([this._fs.queryString, this.refreshList])
+            .pipe(
+                map(([queryString, refresh]) => (queryString && refresh) ? queryString : ''),
+                )
+            .subscribe(queryString => {
+              this.queryDM(queryString);
+            });
+  }
+
+  /**
+   * The query results
+   */
+  get dataResults(): BehaviorSubject<T[]> {
+    return this._dataResults;
+  }
+
+  /**
+   * The dataSource Paginator
+   */
+  get getPaginator(): MatPaginator|null {
+    return this.paginator;
+  }
+  set setPaginator(paginator: MatPaginator|null) {
+    this.paginator = paginator;
+  }
+
+  /**
+   * The dataSource Sort
+   */
+  get getSort(): MatSort|null {
+    return this.sort;
+  }
+  set setSort(sort: MatSort|null) {
+    this.sort = sort;
+  }
+
+  /**
+   * The filterComponent associated to the List
+   */
+  get getFiltersComponent(): SearchFiltersComponent|null {
+    return this._filters.getValue();
+  }
+  set setFiltersComponent(searchFilters: SearchFiltersComponent|null) {
+    this._filters.next(searchFilters);
+  }
+  private _filters: BehaviorSubject<SearchFiltersComponent|null> =
+      new BehaviorSubject<SearchFiltersComponent|null>(null);
+
+  /**
+   * Returns the items displayed on the current list page
+   * @returns {T[]}
+   */
+  getDisplayedItems() {
+    return this.connect().value;
+  }
+
+  /**
+   * Creates and returns a Mango Query from the Filters Component encoded queryString.
+   * Queries the DataModelManager and updates the dataResults
+   *
+   * @param {string} queryString
+   * @returns {DataQueryOptions}
+   */
+  queryDM(queryString: string): DataQueryOptions {
+    if (!queryString) {
+      return {selector: {}};
+    }
+    const filterItems: FilterItem[] = JSON.parse(decodeURI(atob(queryString)));
+    let querySelector = {};
+
+    if (!filterItems.find(f => f.name === 'keyword')) {
+      this.filter = '';
+    }
+    filterItems.forEach(item => {
+      switch (item.name) {
+        case 'keyword':
+          this.filter = item.value.trim().toLowerCase();
+          break;
+        case 'dateStart':
+          if (item && item.value) {
+            this._addNestedProps(
+                querySelector, ['created_at', '$gte'], new Date(item.value).toISOString());
+          }
+          break;
+        case 'dateEnd':
+          if (item && item.value) {
+            this._addNestedProps(
+                querySelector, ['created_at', '$lte'], new Date(item.value).toISOString());
+          }
+          break;
+        default:
+          if (item) {
+            if (!item.isFormData) {
+              this._addNestedProps(
+                  querySelector,
+                  [
+                    item.name.trim().toLowerCase(),
+                    item.operator ? item.operator.value : '$eq',
+                  ],
+                  item.value);
+            } else {
+              this._addNestedProps(
+                  querySelector,
+                  [
+                    `data.${item.name.trim().toLowerCase()}`,
+                    item.operator ? item.operator.value : '$eq',
+                  ],
+                  item.value);
+            }
+          }
+          break;
+      }
+    });
+    const query: DataQueryOptions = {
+      selector: querySelector,
+    };
+    this.getQueryResults(query);
+
+    return query;
+  }
+
+  /**
+   * Queries the dataModelManager and updates the dataResults
+   *
+   * @param {DataQueryOptions} query
+   */
+  getQueryResults(query: DataQueryOptions) {
+    this._dataModelManager.query(query)
+        .pipe(
+            switchMap((rxdbQuery) => {
+              const res = from(rxdbQuery.exec());
+              return res;
+            }),
+            take(1),
+            )
+        .subscribe((results) => {
+          this._dataResults.next(this._rxDocsToJson(results));
+        });
+  }
+
+  /**
+   * Deletes the selected items from the db
+   * @param {T[]} items
+   * @return {T[]}
+   */
+  deleteAction(items: T[]): T[] {
+    let results: RxDocument<T>[]|null = [];
+    this._dataModelManager.bulkDelete(items)
+        .pipe(
+            take(1),
+            )
+        .subscribe(res => {
+          results = res;
+          this.refreshList.next(true);
+        });
+    return this._rxDocsToJson(results);
+  }
+
+  /**
+   * Converts an array of RxDocuments into an array of T objects
+   * @param docs RxDocument[]
+   * @retun T[]
+   */
+  private _rxDocsToJson(docs: RxDocument<T>[]): T[] {
+    let docsJson: T[] = [];
+    docs.forEach(doc => {
+      docsJson.push(doc.toJSON());
+    });
+    return docsJson;
+  }
+
+  /**
+   * Adds a nested object property and an optional value to an object
+   * @param {[key: string]: string|{}} baseObj
+   * @param {string[]} names
+   * @param {any} value?
+   *
+   */
+
+  private _addNestedProps = function(
+      baseObj: {[key: string]: string|{}}, props: string[], value?: any) {
+    let lastProp = arguments.length === 3 ? props.pop() : false;
+
+    for (let i = 0; i < props.length; i++) {
+      baseObj = baseObj[props[i].toString()] = baseObj[props[i].toString()] || {};
+    }
+
+    if (lastProp) {
+      baseObj = baseObj[lastProp.toString()] = value;
+    }
+
+    return baseObj;
+  };
+
+  disconnect() {
+    this._dataResultsSub.unsubscribe();
+    this._filterParamsSub.unsubscribe();
+
+    this._dataResults.complete();
+    this._sortParams.complete();
+    this.refreshList.complete();
+  }
+}
