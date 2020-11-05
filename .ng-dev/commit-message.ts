bfed802f1cff9e5e496584ead177c4d44c3fd67f
trik <marco.marche@gnucoop.com>
2020-11-05 11:04:20 +0100

    ci: set up commit message conformance rules using ng-dev

---
+++ .ng-dev/commit-message.ts
@@ -0,0 +1,14 @@
+import {CommitMessageConfig} from '@angular/dev-infra-private/commit-message/config';
+
+/**
+ * The configuration for `ng-dev commit-message` commands.
+ */
+export const commitMessage: CommitMessageConfig = {
+  maxLineLength: 120,
+  minBodyLength: 0,
+  minBodyLengthTypeExcludes: ['docs'],
+  scopes: [
+    'core/auth',
+    'core/data',
+  ],
+};
