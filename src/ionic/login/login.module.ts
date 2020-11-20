c061b1bafb986e508dcda3c51bea439ccf862344
trik <marco.marche@gnucoop.com>
2020-11-20 13:21:50 +0100

    fix(ionic/login): don't provide child roots in login module
    it will be responsibility of the library user to define a login route

--- src/ionic/login/login.module.ts
+++ src/ionic/login/login.module.ts
@@ -24,7 +24,6 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {ReactiveFormsModule} from '@angular/forms';
-import {RouterModule} from '@angular/router';
 import {IonicModule} from '@ionic/angular';
 
 import {Login} from './login';
@@ -34,7 +33,6 @@ import {Login} from './login';
     CommonModule,
     IonicModule,
     ReactiveFormsModule,
-    RouterModule.forChild([{path: '', component: Login}]),
   ],
   declarations: [
     Login,
