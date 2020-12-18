a9c774626671c6a9a8311311269b5f3b1bb6bc3f
Marco Tozzi <marco.tozzi@gnucoop.com>
2020-12-18 16:35:55 +0100

    feat(material/list): Column Selector component
    Added Column Selector component to material/list package.
    It allows the user to select which table columns to show or hide.

---
+++ src/material/list/columns-selector.ts
@@ -0,0 +1,74 @@
+/**
+ * @license
+ * Copyright (C) Gnucoop soc. coop.
+ *
+ * This file is part of the Dewco (dewco).
+ *
+ * Dewco (dewco) is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * Dewco (dewco) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Dewco (dewco).
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Inject,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {ListHeader} from '@dewco/core/list';
+
+/**
+ * A dialog component that allows to choose which headers and columns to display in the
+ * associated table.
+ */
+@Component({
+  selector: 'dewco-columns-selector',
+  styleUrls: ['columns-selector.css'],
+  templateUrl: 'columns-selector.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+})
+export class ColumnsSelector<T> implements OnInit {
+  /**
+   * The current columns
+   */
+  columns: ListHeader<T>[];
+  constructor(
+      public dialogRef: MatDialogRef<ColumnsSelector<T>>,
+      @Inject(MAT_DIALOG_DATA) public data: {columns: ListHeader<T>[]},
+  ) {}
+
+  ngOnInit() {
+    this.columns = this.data.columns.map(header => ({...header}));
+  }
+
+  /**
+   * Toggles the 'displayed' state of a column/Listheader with the selected index.
+   * An undefined 'displayed' state defaults to true.
+   * @param index
+   */
+  toggleColumn(index: number): void {
+    this.columns[index].displayed =
+        this.columns[index].displayed === undefined ? false : !this.columns[index].displayed;
+  }
+
+  /**
+   * Closes the dialog and updates the displayed headers and columns
+   */
+  apply() {
+    this.dialogRef.close(this.columns);
+  }
+}
