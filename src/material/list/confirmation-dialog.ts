757700bfc821435482adb002e5e91b275c10b1e2
Marco Tozzi <marco.tozzi@gnucoop.com>
2020-12-18 16:39:21 +0100

    feat(material/list): Added Confirmation Dialog component.
    Added a Confirmation Dialog to ask the user to confirm the
    selected action before it is performed.

---
+++ src/material/list/confirmation-dialog.ts
@@ -0,0 +1,55 @@
+/**
+ * @license
+ * Copyright (C) Gnucoop soc. coop.
+ *
+ * This file is part of the Dewco (dewco).
+ *
+ * Dewco (dewco) is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * Dewco (dewco) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Dewco (dewco).
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Inject,
+  ViewEncapsulation,
+} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+
+/**
+ * This Dialog is opened by the AdminUserInteractionsService to ask the user a confirmation
+ * of a ListAction.
+ */
+@Component({
+  selector: 'confirmation-dialog',
+  styleUrls: ['confirmation-dialog.css'],
+  templateUrl: 'confirmation-dialog.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+})
+export class ConfirmationDialog {
+  constructor(
+      public dialogRef: MatDialogRef<ConfirmationDialog>,
+      @Inject(MAT_DIALOG_DATA) public data: any,
+  ) {}
+
+  /**
+   * Closes the dialog returning the user response to the action confirmation request
+   * @param response The user response
+   */
+  confirmationResponse(response: boolean) {
+    this.dialogRef.close(response);
+  }
+}
