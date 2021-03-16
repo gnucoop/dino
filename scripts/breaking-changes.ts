6db7908f6e40b41908bfa337e23c579432a5a4ac
Marco Marche <marco.marche@gnucoop.com>
2021-03-16 12:00:31 +0100

    build: bump chalk from 2.4.2 to 4.1.0
    * build: bump chalk from 2.4.2 to 4.1.0
    
    Bumps [chalk](https://github.com/chalk/chalk) from 2.4.2 to 4.1.0.
    - [Release notes](https://github.com/chalk/chalk/releases)
    - [Commits](https://github.com/chalk/chalk/compare/v2.4.2...v4.1.0)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * build: update import statements for chalk
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- scripts/breaking-changes.ts
+++ scripts/breaking-changes.ts
@@ -1,8 +1,8 @@
-import chalk from 'chalk';
-import {readFileSync} from 'fs';
 import {join, relative} from 'path';
-import * as tsutils from 'tsutils';
+import {readFileSync} from 'fs';
+import * as chalk from 'chalk';
 import * as ts from 'typescript';
+import * as tsutils from 'tsutils';
 
 const projectRoot = process.cwd();
 
