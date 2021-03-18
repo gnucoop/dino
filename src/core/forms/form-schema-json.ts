772123399cb4f6513c102b61cb6fc9435f548d75
trik <marco.marche@gnucoop.com>
2021-03-18 10:13:12 +0100

    build: approve changes to core forms model schema

--- src/core/forms/form-schema-json.ts
+++ src/core/forms/form-schema-json.ts
@@ -43,7 +43,7 @@ export const schema = {
       "description": "The form schema name."
     },
     "schema": {
-      "description": "JSON definition of the Ajf form schema.\nIt must be valid against the [non-scrict\nversion](https://ajf.rocks/schemas/ajf-form-schema.json).",
+      "description": "JSON definition of the Ajf form schema. It must be valid against the [non-scrict version](https://ajf.rocks/schemas/ajf-form-schema.json).",
       "type": "object"
     }
   },
