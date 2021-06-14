6dd3f79b7ff1b65e99524a342b9994f59a2918cf
trik <marco.marche@gnucoop.com>
2021-06-14 13:36:16 +0200

    build: bump Angular dependencies and drop benchpress
    * Updates the `@angular` dependencies to the latest versions.
    * Removes the depenency on `@angular/benchpress` since we weren't using it.

---
+++ tools/public_api_guard/material/breakpoint-observer.d.ts
@@ -0,0 +1,14 @@
+export declare class BreakpointObserverModule {
+    static ɵfac: i0.ɵɵFactoryDeclaration<BreakpointObserverModule, never>;
+    static ɵinj: i0.ɵɵInjectorDeclaration<BreakpointObserverModule>;
+    static ɵmod: i0.ɵɵNgModuleDeclaration<BreakpointObserverModule, never, never, never>;
+}
+
+export declare class BreakpointObserverService {
+    large: Observable<boolean>;
+    medium: Observable<boolean>;
+    small: Observable<boolean>;
+    constructor(_breakPointObserver: BreakpointObserver);
+    static ɵfac: i0.ɵɵFactoryDeclaration<BreakpointObserverService, never>;
+    static ɵprov: i0.ɵɵInjectableDef<BreakpointObserverService>;
+}
