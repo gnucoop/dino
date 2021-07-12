edf3183c4a761575d59e9c6ef7a9a80f4b71840c
Marco Tozzi <marco.tozzi@gnucoop.com>
2021-07-12 14:17:40 +0200

    feat(core/areas): Areas module added.
    Areas optional metric module and model added.
    When imported, augments FormData with an areaId.

---
+++ src/core/areas/area-manager.ts
@@ -0,0 +1,45 @@
+/**
+ * @license
+ * Copyright (C) Gnucoop soc. coop.
+ *
+ * This file is part of the Dewco (dewco).
+ *
+ * Dewco (dewco) is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * Dewco (dewco) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Dewco (dewco).
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import {Injectable} from '@angular/core';
+import {DataModelManager, DataService, PermissionContextService} from '@dewco/core/data';
+
+import {Area} from './area';
+import {schema} from './area-json';
+import {AreasModule} from './areas.module';
+
+/**
+ * Service that manages FormData Locations
+ */
+@Injectable({providedIn: AreasModule})
+export class AreaManager extends DataModelManager<Area> {
+  constructor(
+      dataService: DataService,
+      permissionContextService: PermissionContextService,
+  ) {
+    super(
+        {collection: {name: 'areas', schema}},
+        dataService,
+        permissionContextService,
+    );
+  }
+}
