b185b23414ba7c27cd9b24e6d1973dc542df81ba
trik <marco.marche@gnucoop.com>
2021-10-14 09:35:07 +0200

    build: remove ngcc compatibility CI test jobs
    Removes all the ngcc compatibility CI jobs as the APF v13 output
    no longer comes with View Engine output and makes ngcc a noop for the Dewco package output.

--- scripts/local-pipeline.sh
+++ scripts/local-pipeline.sh
@@ -28,15 +28,6 @@ echo "Build release packages"
 yarn build-and-check-release-output
 yarn check-tooling-setup
 
-echo "Ngcc compatibility check"
-rm -Rf node_modules/@dewco
-mkdir -p node_modules/@dewco
-cp -R dist/releases/* node_modules/@dewco/
-mv node_modules/__ngcc_entry_points__.json node_modules/__ngcc_entry_points__.json.back
-yarn ngcc --error-on-failed-entry-point --no-tsconfig
-mv node_modules/__ngcc_entry_points__.json.back node_modules/__ngcc_entry_points__.json
-rm -Rf node_modules/@dewco
-
 echo "Build"
 "${BAZEL_BINARY}" build --build_tag_filters=-docs-package,-release-package -- src/...
 
