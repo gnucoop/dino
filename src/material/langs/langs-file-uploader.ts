4d92d5157e0c0aa9b9b0cfe36af055b4a98c5f1e
trik <marco.marche@gnucoop.com>
2022-03-25 16:29:32 +0100

    style(material/langs): change license headers

--- src/material/langs/langs-file-uploader.ts
+++ src/material/langs/langs-file-uploader.ts
@@ -2,20 +2,20 @@
  * @license
  * Copyright (C) Gnucoop soc. coop.
  *
- * This file is part of the Dewco Core (dewco).
+ * This file is part of the Dino (dino).
  *
- * Dewco Core (dewco) is free software: you can redistribute it and/or
+ * Dino (dino) is free software: you can redistribute it and/or
  * modify it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
  *
- * Dewco Core (dewco) is distributed in the hope that it will be useful,
+ * Dino (dino) is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
  * General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with Dewco Core (dewco).
+ * along with Dino (dino).
  * If not, see http://www.gnu.org/licenses/.
  *
  */
