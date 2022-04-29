fe73a6bb64d6b342ba4b5a8c9d01aa7b6538690f
trik <marco.marche@gnucoop.com>
2022-04-29 09:10:48 +0200

    docs: fix edit report e2e test execution

--- projects/material/cypress/integration/edit-form.e2e.spec.ts
+++ projects/material/cypress/integration/edit-form.e2e.spec.ts
@@ -1,3 +1,5 @@
+import {formDatas} from '../../../e2e-app/src/test-ajf-formdata';
+
 describe('dino-edit-form', () => {
   beforeEach(() => cy.visit('/forms'));
 
@@ -5,8 +7,14 @@ describe('dino-edit-form', () => {
     cy.get('dino-collect').should('exist');
     cy.get('mat-grid-tile').should('exist').first().click();
     cy.get('dino-list').should('exist');
-    cy.get('mat-row').should('exist').first().click();
-    cy.get('.mat-cell.dino-row-actions .mat-icon.mat-list-icon').first().click();
+    cy.get('mat-row:not(.dino-row-details)')
+      .should('have.length', formDatas.length)
+      .first()
+      .invoke('addClass', 'dino-hover');
+    cy.get('mat-row .mat-cell.dino-row-actions .mat-icon.mat-list-icon')
+      .first()
+      .invoke('mouseover')
+      .click();
     cy.get('dino-edit-form').should('exist');
     cy.url().should('contain', 'forms').should('contain', 'edit');
   });
--- projects/material/cypress/integration/edit-report.e2e.spec.ts
+++ projects/material/cypress/integration/edit-report.e2e.spec.ts
@@ -1,3 +1,5 @@
+import {reportDatas} from '../../../e2e-app/src/test-ajf-reportdata';
+
 describe('dino-edit-report', () => {
   beforeEach(() => cy.visit('/reports'));
 
@@ -5,8 +7,15 @@ describe('dino-edit-report', () => {
     cy.get('dino-collect').should('exist');
     cy.get('mat-grid-tile').should('exist').first().click();
     cy.get('dino-list').should('exist');
-    cy.get('mat-row').should('exist').first().click();
-    cy.get('.mat-cell.dino-row-actions .mat-icon.mat-list-icon').first().next().click();
+    cy.get('mat-row:not(.dino-row-details)')
+      .should('have.length', reportDatas.length)
+      .first()
+      .invoke('addClass', 'dino-hover');
+    cy.get('mat-row .mat-cell.dino-row-actions .mat-icon.mat-list-icon')
+      .first()
+      .next()
+      .invoke('mouseover')
+      .click();
     cy.get('dino-edit-report').should('exist');
     cy.url().should('contain', 'reports').should('contain', 'view');
   });
