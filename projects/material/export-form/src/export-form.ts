e9265f779a84c99b887abddcf700e9a82dfcd729
Marco Tozzi <marco.tozzi@gnucoop.com>
2022-07-26 10:44:13 +0200

    feat(material/export-form): Form Status is now exported

--- projects/material/export-form/src/export-form.ts
+++ projects/material/export-form/src/export-form.ts
@@ -365,6 +365,11 @@ export class ExportForm implements AfterViewInit, OnDestroy {
                   }
                 }
               });
+              if (ctx.dino['form_status']) {
+                exportCtx[`form_status_name`] = ctx.dino['form_status']['name'];
+                exportCtx[`form_status_label`] = ctx.dino['form_status']['label'];
+                exportCtx[`form_status_level`] = ctx.dino['form_status']['status_level'];
+              }
               exportCtxList.push(exportCtx);
             }
           });
