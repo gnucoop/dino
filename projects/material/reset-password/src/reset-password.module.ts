1c14f3a7217b1ad30bec1267f8de79e2831dfd39
Marco Tozzi <marco.tozzi@gnucoop.com>
2022-09-14 14:50:56 +0200

    lint: Minor lint fixes

--- projects/material/reset-password/src/reset-password.module.ts
+++ projects/material/reset-password/src/reset-password.module.ts
@@ -25,8 +25,6 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {ReactiveFormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatExpansionModule} from '@angular/material/expansion';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
