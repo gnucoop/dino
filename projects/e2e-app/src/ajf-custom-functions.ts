7bba01f8ece27b55a7629269457cb7b8ee45453e
Sara <sara.repossi@gmail.com>
2024-02-26 14:32:29 +0100

    feat(e2e-material): add common ajf custom functions

--- projects/e2e-app/src/ajf-custom-functions.ts
+++ projects/e2e-app/src/ajf-custom-functions.ts
@@ -2,34 +2,41 @@ import {AjfCustomFunctions} from '@dino/core/data';
 import {FormInfo} from '@dino/core/forms';
 
 export function readonly(stepLevel: number, info: FormInfo | null): boolean {
-  return false;
-  /*
   if (info == null) {
     return stepLevel == 0 ? false : true;
   }
   const currentStatusLevel = info?.status?.status_level ?? 0;
-  return currentStatusLevel >= stepLevel;*/
+  return currentStatusLevel >= stepLevel;
 }
 export function visibility(stepLevel: number, info: FormInfo | null): boolean {
-  return true;
-  /*
   if (info == null) {
     return stepLevel == 0 ? true : false;
   }
   const currentStatusLevel = info?.status?.status_level ?? 0;
 
-  return currentStatusLevel >= stepLevel - 1;*/
+  return currentStatusLevel >= stepLevel - 1;
 }
 
 export function isUserInGroup(groupName: string, info: FormInfo | null): boolean {
-  if (groupName !== 'Admin') {
-    return false;
+  let allUserGroups: string[] = [];
+  if (groupName && info && info.activeUserGroups) {
+    allUserGroups = info.activeUserGroups.map(g => g.groupName);
   }
-  return true;
+  return allUserGroups.includes(groupName);
+}
+
+// Is a user in at least one group of the list
+export function isUserInAtLeastOneGroup(groupNames: string[], info: FormInfo | null): boolean {
+  let allUserGroups: string[] = [];
+  if (groupNames && info && info.activeUserGroups) {
+    allUserGroups = info.activeUserGroups.map(g => g.groupName);
+  }
+  return allUserGroups.some(ug => groupNames.includes(ug));
 }
 
 export const ajfCustomFunctions: AjfCustomFunctions = {
   readonly,
   visibility,
   isUserInGroup,
+  isUserInAtLeastOneGroup,
 };
