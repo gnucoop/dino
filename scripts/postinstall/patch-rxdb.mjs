b03184e5241f2b56bbdf17011e6df6a65fae90b5
Marco Tozzi <marco.tozzi@gnucoop.com>
2024-07-08 16:40:02 +0200

    scripts: Changed patchRxDb postinstall script. Applies to the new rxDb version (15.26.0).

--- scripts/postinstall/patch-rxdb.mjs
+++ scripts/postinstall/patch-rxdb.mjs
@@ -2,12 +2,11 @@ import {default as esMain} from 'es-main';
 import {readFileSync, writeFileSync} from 'fs';
 
 export const patchRxDb = () => {
-  const patchFile = 'node_modules/rxdb/dist/es/plugins/replication/index.js';
-
-  const content = readFileSync(patchFile, 'utf-8').replace(
-    'result = _this2$push$handler',
-    'result = []',
-  );
+  // const patchFile = 'node_modules/rxdb/dist/es/plugins/replication/index.js';
+  const patchFile = 'node_modules/rxdb/dist/types/types/util.d.ts';
+  const content = readFileSync(patchFile, 'utf-8')
+    .replace('Paths<T, D extends number = 10>', 'Paths<T, D extends number = 3>')
+    .replace('Leaves<T, D extends number = 10>', 'Leaves<T, D extends number = 3>');
   writeFileSync(patchFile, content);
 };
 
