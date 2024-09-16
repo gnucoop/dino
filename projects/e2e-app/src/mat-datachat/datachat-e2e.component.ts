582d4b3fb6bcbc759d4b02c6191fd49d0c845883
Marco Tozzi <marco.tozzi@gnucoop.com>
2024-09-16 09:37:26 +0200

    refactor(e2e-app/datachat): Added urls for API and endpoints. Added template inputs.

--- projects/e2e-app/src/mat-datachat/datachat-e2e.component.ts
+++ projects/e2e-app/src/mat-datachat/datachat-e2e.component.ts
@@ -1,9 +1,21 @@
 import {Component} from '@angular/core';
-
+import {ajfCustomFunctions} from '../ajf-custom-functions';
 @Component({
   selector: 'app-datachat',
   templateUrl: './datachat-e2e.component.html',
 })
 export class DataChatE2E {
+  ajfCustomFunctions = ajfCustomFunctions;
+  baseDataChatAPIurl = 'http://127.0.0.1:5000';
+  validateEndpoint = 'validateapikey';
+  startEndpoint = 'startdatachat';
+  endEndpoint = 'enddatachat';
+  chatEndpoint = 'datachat';
+  endpointUrls = {
+    validateEndpoint: this.validateEndpoint,
+    startEndpoint: this.startEndpoint,
+    endEndpoint: this.endEndpoint,
+    chatEndpoint: this.chatEndpoint,
+  };
   constructor() {}
 }
