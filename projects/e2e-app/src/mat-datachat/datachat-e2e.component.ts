91e082a49f02ff4923973ddcb5a9b5e08bf1f9f9
Marco Tozzi <marco.tozzi@gnucoop.com>
2024-12-03 12:19:26 +0100

    refactor(e2e-app/datachat): Added terms text in datachat.

--- projects/e2e-app/src/mat-datachat/datachat-e2e.component.ts
+++ projects/e2e-app/src/mat-datachat/datachat-e2e.component.ts
@@ -17,5 +17,52 @@ export class DataChatE2E {
     endEndpoint: this.endEndpoint,
     dataChatEndpoint: this.dataChatEndpoint,
   };
+  acceptTermsContent = `
+  <h2>Condizioni di utilizzo del servizio DinoGPT</h2>
+  <p>
+    DinoGPT è un assistente virtuale basato sulla tecnologia di ChatGPT, cioè un programma informatico
+    creato per fornire informazioni e assistenza all'utente in base alle conoscenze reperite all'interno
+    di un database specifico, definito dal programmatore del software.
+  </p>
+  <p>
+    Le risposte e le informazioni fornite da DinoGPT sono, quindi, generate in base a modelli statistici
+    e al testo di input fornito dal programmatore.
+  </p>
+  <p>
+    È importante notare che le informazioni e risposte fornite da DinoGPT all'utente potrebbero
+    non essere sempre accurate o aggiornate, poiché la conoscenza di DinoGPT è limitata al contenuto
+    del database a cui ha accesso.
+  </p>
+  <p>
+    DinoGPT non ha accesso a Internet o a fonti esterne diverse dal database fornitogli dal programmatore e,
+    quindi, le risposte fornite potrebbero non essere aggiornate alla data di consultazione da parte
+    dell'utente, che è tenuto a verificarle autonomamente, con esonero da qualsivoglia responsabilità
+    da parte del programmatore.
+  </p>
+  <p>
+    Si precisa, inoltre, che le informazioni fornite da DinoGPT non devono mai essere considerate
+    alla stregua di una consulenza legale o, in genere, professionale. È, quindi, sempre fondamentale che,
+    una volta reperita l'informazione richiesta, l'utente si consulti con un esperto o un professionista
+    qualificato per ricevere consulenza specifica in merito alle proprie esigenze.
+  </p>
+  <p>
+    L'utente è l'unico responsabile del corretto utilizzo delle informazioni reperite tramite DinoGPT,
+    che deve sempre essere utilizzato in modo consapevole e diligente in funzione delle caratteristiche
+    tipiche degli assistenti virtuali.
+  </p>
+  <p>
+    Il programmatore non assume alcuna responsabilità per eventuali perdite o danni, di qualsivoglia
+    genere o specie, derivanti dall'uso delle informazioni fornite da DinoGPT.
+  </p>
+  <p>
+    Utilizzando questo servizio, l'utente accetta e comprende i termini e le condizioni di cui sopra
+    con esonero di qualsivoglia responsabilità in capo al programmatore. Se non si accettano questi termini,
+    si prega di astenersi dall'utilizzare DinoGPT.
+  </p>
+  <p>
+    Questo disclaimer è soggetto a modifiche e aggiornamenti in funzione degli sviluppi del servizio.
+    È responsabilità dell'utente verificare periodicamente eventuali modifiche alle presenti condizioni
+    di servizio.
+  </p>`;
   constructor() {}
 }
