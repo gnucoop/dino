40bfbf6ab0c9a4821263cc8a5aa62e2170ec4cd4
Marco Tozzi <marco.tozzi@gnucoop.com>
2026-03-19 11:33:30 +0100

    refactor(core/forms): FormDataManager method "compareFormDatasData" now assesses object equality correctly when comparing forms datas.

--- projects/core/forms/src/form-data-manager.ts
+++ projects/core/forms/src/form-data-manager.ts
@@ -134,15 +134,59 @@ export class FormDataManager extends DataModelManager<FormData> {
   compareFormDatasData(data_1: {[key: string]: any}, data_2: {[key: string]: any}): string[] {
     const changedAttributes: string[] = [];
     for (let key in data_2) {
-      if (data_1[key] != data_2[key] && !key.startsWith('$')) {
-        if (!Array.isArray(data_2[key]) && !Array.isArray(data_1[key])) {
-          changedAttributes.push(key);
-        } else if (!this._areArraysEquivalent(data_1[key], data_2[key])) {
-          changedAttributes.push(key);
+      if (key.startsWith('$')) continue;
+      if (typeof data_1[key] === 'object' && typeof data_2[key] === 'object') {
+        if (!this.areObjectsEqual(data_1[key], data_2[key])) changedAttributes.push(key);
+      } else {
+        if (data_1[key] != data_2[key]) {
+          if (!Array.isArray(data_2[key]) && !Array.isArray(data_1[key])) {
+            changedAttributes.push(key);
+          } else if (!this._areArraysEquivalent(data_1[key], data_2[key])) {
+            changedAttributes.push(key);
+          }
         }
       }
     }
     return changedAttributes;
+  }
+
+  /**
+   * Compares two objects for deep equality
+   * @param obj1
+   * @param obj2
+   * @returns True if the objects are equal
+   */
+  areObjectsEqual(obj1: any, obj2: any): boolean {
+    if (obj1 === obj2) return true;
+
+    if (obj1 === null || typeof obj1 !== 'object' || obj2 === null || typeof obj2 !== 'object') {
+      return false;
+    }
+
+    if (obj1 instanceof Date && obj2 instanceof Date) {
+      return obj1.getTime() === obj2.getTime();
+    }
+
+    if (Array.isArray(obj1) && Array.isArray(obj2)) {
+      if (obj1.length !== obj2.length) return false;
+      for (let i = 0; i < obj1.length; i++) {
+        if (!this.areObjectsEqual(obj1[i], obj2[i])) return false; // Recursive call
+      }
+      return true;
+    }
+
+    const keys1 = Object.keys(obj1);
+    const keys2 = Object.keys(obj2);
+
+    if (keys1.length !== keys2.length) return false;
+
+    for (const key of keys1) {
+      if (!keys2.includes(key) || !this.areObjectsEqual(obj1[key], obj2[key])) {
+        return false;
+      }
+    }
+
+    return true;
   }
 
   /**
